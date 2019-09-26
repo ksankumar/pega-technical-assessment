2bd5cfe0a1a854e1d900404bf5bdd314097e61ba
Santhosh Kumar Krishnan <ksankumar@gmail.com>
2019-09-26 17:38:04 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
+/**
+ * index.js Created by sandy on 9/26/2019 5.26PM
+ */
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
